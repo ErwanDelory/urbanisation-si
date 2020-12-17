1c796417607f39a997502738126325e91d089107
Dordb <43724442+Dordb@users.noreply.github.com>
2020-12-17 12:13:23 +0100

    playlist controllers

--- nodemongo/api/controllers/playlistControllers.js
+++ nodemongo/api/controllers/playlistControllers.js
@@ -13,14 +13,70 @@ const playlist = require("../model/playlist");
         },
     });
 */
+//Video
+async function addVideoPlaylist(req, res, next) {
+	let playlist;
+	const playlistId = req.params.pid;
+	const url = req.body.url;
+	try {
+		playlist = await Playlist.findById(playlistId);
+		playlist.urls.push(url);
+		playlist = await playlist.save();
+	} catch (err) {
+		return next(
+			new Error("Something went wrong, could not find this playlist", 404)
+		);
+	}
+	if (!playlist) {
+		return next(new Error("Error, Playlist not found", 404));
+	}
+	res.json({ playlist: playlist.toObject({ getters: true }) });
+}
 
-async function addVideoPlaylist(req, res, next) {}
+async function rmVideoPlaylist(req, res, next) {
+	let playlist;
+	const playlistId = req.params.pid;
+	const url = req.body.url;
+	try {
+		playlist = await Playlist.findById(playlistId);
+	} catch (err) {
+		return next(new Error("Error, Playlist not found", 404));
+	}
 
-async function rmVideoPlaylist(req, res, next) {}
+	index = playlist.urls.indexOf(url);
+	if (index === -1) {
+		return next(new Error("Url not in the playlist", 404));
+	}
+	playlist.urls.splice(index, 1);
+	try {
+		playlist = await playlist.save();
+	} catch (err) {
+		return next(
+			new Error("Something went wrong, could not save change", 404)
+		);
+	}
+	res.json({ playlist: playlist.toObject({ getters: true }) });
+}
 
+// Playlist
 async function updatePlaylist(req, res, next) {}
 
-async function deletePlaylist(req, res, next) {}
+async function deletePlaylist(req, res, next) {
+	const playlistId = req.params.pid;
+	let playlist;
+	try {
+		playlist = await Playlist.findOneAndRemove({ _id: playlistId });
+		console.log(playlist);
+	} catch (err) {
+		return next(
+			new Error("Something went wrong, could not find this playlist", 404)
+		);
+	}
+	if (!playlist) {
+		return next(new Error("Error, Playlist not found", 404));
+	}
+	res.json({ playlist: playlist.toObject({ getters: true }) });
+}
 
 async function getPlaylistById(req, res, next) {
 	let playlist;
@@ -40,15 +96,16 @@ async function getPlaylistById(req, res, next) {
 
 async function getPlaylistByCreator(req, res, next) {
 	const userId = req.params.uid;
+	if (userId === "") {
+		return next(new Error("Error, no userId", 404));
+	}
 	let playlist;
 	try {
-		playlist = await Playlist.find({ creator: userId });
+		playlist = await Playlist.find({ "info.creator": userId });
 	} catch (err) {
 		return next(new Error("Something went wrong, could not find", 404));
 	}
-	if (!playlist) {
-		return next(new Error("Error, Playlist not found for userId", 404));
-	}
+
 	res.json({
 		playlist: playlist.map((playlist) =>
 			playlist.toObject({ getters: true })
@@ -83,6 +140,7 @@ async function createPlaylist(req, res, next) {
 
 exports.addVideoPlaylist = addVideoPlaylist;
 exports.rmVideoPlaylist = rmVideoPlaylist;
+
 exports.updatePlaylist = updatePlaylist;
 exports.deletePlaylist = deletePlaylist;
 exports.getPlaylistById = getPlaylistById;
--- nodemongo/api/routes/routes.js
+++ nodemongo/api/routes/routes.js
@@ -3,8 +3,14 @@ const playlistControllers = require("../controllers/playlistControllers");
 
 const router = express.Router();
 
-router.get("/playlist/user/:pid", playlistControllers.getPlaylistById);
-router.get("/playlist/creator/:uid", playlistControllers.getPlaylistById);
+// *** Playlist Controllers *** \\\
+// Video
+router.patch("/playlist/add/:pid", playlistControllers.addVideoPlaylist);
+router.patch("/playlist/remove/:pid", playlistControllers.rmVideoPlaylist);
+//Playliste
+router.delete("/playlist/delete/:pid", playlistControllers.deletePlaylist);
+router.get("/playlist/find/:pid", playlistControllers.getPlaylistById);
+router.get("/playlist/creator/:uid", playlistControllers.getPlaylistByCreator);
 router.post("/playlist/new", playlistControllers.createPlaylist);
 
 module.exports = router;
